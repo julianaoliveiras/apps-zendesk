0eccdf714778fe45ff0bd09cd9a94c0d47ee072c
Juliana Oliveira <h4bituei.o@gmail.com>
2022-03-22 11:19:31 -0300

    app consultation cpf finalized

--- Consulta CPF/assets/controller/main.controller.js
+++ Consulta CPF/assets/controller/main.controller.js
@@ -1,4 +1,32 @@
 angularjs.controller('angularjs', ['$scope', 'zendeskService', function($scope, zendeskService){
+   
+    $scope.ocultar = function(){
+        return $scope.ocultar ? true: false;
+    };
     
+    $scope.consultar = function(){
+        $scope.dadosCliente= [];
+        if($scope.consultaCPF.length == 11){
+            zendeskService.pegarCPF($scope.consultaCPF).then(function (response){
+                $scope.oculta = false;
+                responseCPF = JSON.parse(response)
+                let pedidos = responseCPF.pedidos
+                console.log($scope.dadosCliente)
+                for (let i=0; i<pedidos.length; i++){
+                    zendeskService.pegarPedido(pedidos[i]).then(function(response){
+                        $scope.oculta = true;
+                        $scope.dadosCliente.push(response)
+                        console.log($scope.dadosCliente)
+                    });
+                }
+            })
+        }else if($scope.consultaCPF.length<11){
+            zendeskService.pegarPedido($scope.consultaCPF).then(function (response){
+                $scope.oculta= true;
+                $scope.dadosCliente.push(response)
+                console.log($scope.dadosCliente)
+            })
+        }
+    }
     
 }]);
--- Consulta CPF/assets/service/zendesk.service.js
+++ Consulta CPF/assets/service/zendesk.service.js
@@ -1,62 +1,48 @@
+//Chamada da api para dentro do zedndesk
+
 (function (){
     'use strict';
     
-    angularjs.service('zendeskService', ["$q", function($q) {
+    angularjs.service('zendeskService', ["$q", function($q){
         
         var client = ZAFClient.init();
        
-        return {
-            listarUsers: function(){
-                var deferred = $q.defer();
-                client.request({
-                    url: '/api/v2/users',
-                    type: 'GET',
-                    contentType: 'application/json'
-                }).then(function (response){
-                    deferred.resolve(response);
-                }).catch(function (error){
-                    deferred.reject(error);
-                });
-                return deferred.promise;
-            },
-            capturaCPF: function (cpf) {
+        return{
+            pegarCPF: function(cpf){
                 var deferred = $q.defer();
                 let options = {
                     url: "https://n0e6xtbvqa.execute-api.us-east-1.amazonaws.com/default/flordemaio-zendesk-mock?cpf=" + cpf,
                     cors: false,
                     method: "GET",
                     headers: {
-                        "x-api-key": "JBjAVMqtRovzYJ8iRJm559U5mLn9Hwl7X5iVCvm9"
+                        "x-api-key":
+                        "JBjAVMqtRovzYJ8iRJm559U5mLn9Hwl7X5iVCvm9"
                     },
-
                 };
-                client.request(options)
-                    .then(function (data) {
-                        deferred.resolve(data);
-                    }).catch(function (error) {
-                        deferred.reject(error);
-                    });
+                client.request(options).then(function (data){
+                    deferred.resolve(data);
+                }).catch(function(error){
+                    deferred.reject(error);
+                });
                 return deferred.promise;
             },
-            capturaPedido: function (cpf) {
+            pegarPedido: function(cpf){
                 var deferred = $q.defer();
-
                 let options = {
-                    url: "https://v4c1btaq78.execute-api.us-east-1.amazonaws.com/default/allShopping?idShopping=" + cpf,
+                    url:"https://v4c1btaq78.execute-api.us-east-1.amazonaws.com/default/allShopping?idShopping=" + cpf,
                     cors: false,
                     method: "GET",
-                    headers: {
+                    headers:{
                         "x-api-key": "Q8umEmH6Pa2sn2qh1V0AT7SegPfcNtbd2iXSIfyX"
                     },
                 };
-                client.request(options)
-                    .then(function (data) {
-                        deferred.resolve(data);
-                    }).catch(function (error) {
-                        deferred.reject(error);
-                    });
+                client.request(options).then(function (data){
+                    deferred.resolve(data);
+                }).catch(function(error){
+                    deferred.reject(error);
+                });
                 return deferred.promise;
             }
         }
-    }])
+    }]);
 })();
